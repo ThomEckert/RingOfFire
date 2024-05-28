c25995288f73fe1abc1e5141d201524977455582
ThomEckert <thec.lernt@gmx.de>
2024-05-28 14:34:16 +0200

    blubber

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -10,17 +10,17 @@ import {MatCardModule} from '@angular/material/card';
 })
 export class GameInfoComponent implements OnInit, OnChanges {
   cardAction = [
-    { title: 'Wasserfall', description: 'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 may stop drinking. Player 3 may stop as soon as player 2 stops drinking, and so on.' },
-    { title: 'Du', description: 'You decide who drinks' },
-    { title: 'Ich', description: 'Congrats! Drink a shot!' },
+    { title: 'Waterfall', description: 'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 may stop drinking. Player 3 may stop as soon as player 2 stops drinking, and so on.' },
+    { title: 'You', description: 'You decide who drinks' },
+    { title: 'Me', description: 'Congrats! Drink a shot!' },
     { title: 'Category', description: 'Come up with a category (e.g. Colors). Each player must enumerate one item from the category.' },
     { title: 'Bust a jive', description: 'Player 1 makes a dance move. Player 2 repeats the dance move and adds a second one. ' },
     { title: 'Chicks', description: 'All girls drink.' },
     { title: 'Heaven', description: 'Put your hands up! The last player drinks!' },
     { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drink and the other way around.' },
-    { title: 'Thumbmaster', description: '' },
+    { title: 'Thumbmaster', description: 'Touch the tabletop with your thumb. The last player to touch the table has to drink.' },
     { title: 'Men', description: 'All men drink.' },
-    { title: 'Quizmaster', description: '' },
+    { title: 'Rhyme', description: 'Pick a word. In a clockwise direction, the other players have to figure it out. Anyone who repeats a word or cannot find a new rhyme has to drink' },
     { title: 'Never have i ever...', description: 'Say something you nnever did. Everyone who did it has to drink.' },
     { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.' },
   ];
